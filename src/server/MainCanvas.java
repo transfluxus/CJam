10fa6632131712b31a7359c153c6c47eb504acb1
Ramin S <diskordier@gmail.com>
2014-11-09 13:34:34 +0100

    main?

--- src/server/MainCanvas.java
+++ src/server/MainCanvas.java
@@ -32,5 +32,7 @@ public class MainCanvas extends PApplet {
 
 	public class blob_127_0_0_1 extends CJamBlob {
 public void setup() {  background(0);}public void draw() {  fill(255,100,0);  ellipse(sin(frameCount*0.01f)*width/2, height/2, 30, 30);}public void mousePressed() {}
+}public class blob_192_168_2_113 extends CJamBlob {
+import client.*;public void setup() {  size(800,600); println(cj.log);background(0);  fill(255);}public void draw() {  ellipse(random(width),height/2+random(height/10),10,10);}
 }
 }
